4222a68d6c86d8d47a7e6a144b6b1dd0471b8dfc
Jin Choi <choij@cern.ch>
2024-09-13 20:36:31 +0900

    Add DY01234j/4j with simplified version

--- bin/MadGraph5_aMCatNLO/cards/13p6TeV/mg4gpu/test.sh
+++ bin/MadGraph5_aMCatNLO/cards/13p6TeV/mg4gpu/test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-for j in 0j 1j 2j 3j; do
-	for backend in FORTRAN CPP CUDA; do
-		file_path="DY${j}_LO_5f_${backend}/DY${j}_LO_5f_${backend}_proc_card.dat"
-		echo $file_path
-		cat $file_path
-
-	done
+for j in 4j 01234j; do
+  for backend in FORTRAN CPP CUDA; do
+      cp -r DY${j}_LO_5f_${backend} DY${j}_LO_5f_Simplified_${backend}
+      cd DY${j}_LO_5f_Simplified_${backend}
+      mv DY${j}_LO_5f_${backend}_proc_card.dat DY${j}_LO_5f_Simplified_${backend}_proc_card.dat
+      mv DY${j}_LO_5f_${backend}_run_card.dat DY${j}_LO_5f_Simplified_${backend}_run_card.dat
+      mv DY${j}_LO_5f_${backend}_customizecards.dat DY${j}_LO_5f_Simplified_${backend}_customizecards.dat
+      mv DY${j}_LO_5f_${backend}_set_nb_core_32.patch DY${j}_LO_5f_Simplified_${backend}_set_nb_core.patch
+      cd -
+    done
 done
