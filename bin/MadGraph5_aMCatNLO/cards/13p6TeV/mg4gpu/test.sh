cdd585bc4ceae3fcb41c9c6bf4ff2571366a80a9
Jin Choi <choij@cern.ch>
2024-09-23 15:39:11 +0900

    Add NSimplified for DY+4j/01234j with adding initial parton d d~

--- bin/MadGraph5_aMCatNLO/cards/13p6TeV/mg4gpu/test.sh
+++ bin/MadGraph5_aMCatNLO/cards/13p6TeV/mg4gpu/test.sh
@@ -2,12 +2,12 @@
 
 for j in 4j 01234j; do
   for backend in FORTRAN CPP CUDA; do
-      cp -r DY${j}_LO_5f_${backend} DY${j}_LO_5f_Simplified_${backend}
-      cd DY${j}_LO_5f_Simplified_${backend}
-      mv DY${j}_LO_5f_${backend}_proc_card.dat DY${j}_LO_5f_Simplified_${backend}_proc_card.dat
-      mv DY${j}_LO_5f_${backend}_run_card.dat DY${j}_LO_5f_Simplified_${backend}_run_card.dat
-      mv DY${j}_LO_5f_${backend}_customizecards.dat DY${j}_LO_5f_Simplified_${backend}_customizecards.dat
-      mv DY${j}_LO_5f_${backend}_set_nb_core_32.patch DY${j}_LO_5f_Simplified_${backend}_set_nb_core.patch
+      cp -r DY${j}_LO_5f_${backend} DY${j}_LO_5f_NSimplified_${backend}
+      cd DY${j}_LO_5f_NSimplified_${backend}
+      mv DY${j}_LO_5f_${backend}_proc_card.dat DY${j}_LO_5f_NSimplified_${backend}_proc_card.dat
+      mv DY${j}_LO_5f_${backend}_run_card.dat DY${j}_LO_5f_NSimplified_${backend}_run_card.dat
+      mv DY${j}_LO_5f_${backend}_customizecards.dat DY${j}_LO_5f_NSimplified_${backend}_customizecards.dat
+      mv DY${j}_LO_5f_${backend}_set_nb_core_*.patch DY${j}_LO_5f_NSimplified_${backend}_set_nb_core.patch
       cd -
     done
 done
